feb7358904e232b132e8b8a34f192c430f0d02b6
Your Name <you@example.com>
2023-06-07 14:31:05 +0300

    Final

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Вебинар наставника
 load_dotenv()
 # load_dotenv(os.path.join(BASE_DIR.parent.parent, 'infra/.env'), verbose=True)
 
